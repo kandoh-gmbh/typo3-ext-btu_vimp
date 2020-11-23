8dbba08ce9fffedfbd38ab9225e523c0ac3fe5e8
Thomas Scholze <thomas.scholze@gmail.com>
2020-11-23 13:24:24 +0100

    [TASK] update dependencies and set version to 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Thomas Scholze',
     'author_email' => 'thomas.scholze@b-tu.de',
     'state' => 'stable',
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-          'typo3' => '8.7.13-9.5.99',
+          'typo3' => '9.5.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
